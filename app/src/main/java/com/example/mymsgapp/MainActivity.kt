19827e70c093cf6d907e4318f9b66184a631bf1a
michaelJoep <michaelp4great@gmail.com>
2019-09-09 01:57:44 +0100

    MyAppMsg

---
+++ app/src/main/java/com/example/mymsgapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mymsgapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Toast.makeText(this, "Show Toast was clicked", Toast.LENGTH_LONG).show()
+        }
+        btnSendMsg.setOnClickListener {
+            val message = msgtext.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
